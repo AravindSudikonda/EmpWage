ddfda08d45a4f34cafde921202c93e5c96ae5379
AravindSudikonda <aravindsudikonda10@example.com>
2022-05-18 16:45:59 +0530

    added welcome message

---
+++ src/com/bridgelabz/empwage/EmpWageUC1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.empwage;
+
+public class EmpWageUC1 {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("welcome to empwage pgm");
+    }
+}
