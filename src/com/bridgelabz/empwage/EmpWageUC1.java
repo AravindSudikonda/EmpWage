7c98eae8b05c5e1ea981d592fa4e438fd3b01490
AravindSudikonda <aravindsudikonda10@example.com>
2022-05-18 16:52:07 +0530

    Added pgm to check Employe present r absent

--- src/com/bridgelabz/empwage/EmpWageUC1.java
+++ src/com/bridgelabz/empwage/EmpWageUC1.java
@@ -5,5 +5,11 @@ public class EmpWageUC1 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("welcome to empwage pgm");
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
